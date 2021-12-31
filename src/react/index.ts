23858be82982ebc7c4e2c3d07ea729a62ff811e4
Federico Baldini <federicobaldini@192.168.1.57>
2021-12-31 01:22:45 +0100

    move types in types folder

--- src/react/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './DefinedReactNode.type';
-export * from './WithChildren.type';
-export * from './WithClassName.type';
-export * from './WithOptionalChildren.interface';
-export * from './WithOptionalClassName.interface';
-export * from './WithOptionalTestId.interface';
