b1ab554ecc893c7a064c246b35945c90fcc214f9
Federico Baldini <federicobaldini@192.168.1.57>
2022-03-18 00:02:46 +0100

    add linter

--- src/types/DeepNullable.type.ts
+++ src/types/DeepNullable.type.ts
@@ -4,11 +4,9 @@ import { PlainObject } from "./PlainObject.type";
 export type DeepNullable<Type> = Type extends PlainObject
   ? Type extends Array<any>
     ? Nullable<Type>
-    : Nullable<
-        {
-          [Property in keyof Type]: Type[Property] extends Array<any>
-            ? Nullable<Type[Property]>
-            : DeepNullable<Type[Property]>;
-        }
-      >
+    : Nullable<{
+        [Property in keyof Type]: Type[Property] extends Array<any>
+          ? Nullable<Type[Property]>
+          : DeepNullable<Type[Property]>;
+      }>
   : Nullable<Type>;
--- src/types/DeepOmit.type.ts
+++ src/types/DeepOmit.type.ts
@@ -1,6 +1,8 @@
-export type DeepOmit<Type, Keys extends string | number | symbol> =
-  Type extends Array<any>
-    ? { [index: number]: Omit<Type[number], Keys> }
-    : Type extends object
-    ? Omit<{ [Property in keyof Type]: DeepOmit<Type[Property], Keys> }, Keys>
-    : Omit<Type, Keys>;
+export type DeepOmit<
+  Type,
+  Keys extends string | number | symbol
+> = Type extends Array<any>
+  ? { [index: number]: Omit<Type[number], Keys> }
+  : Type extends object
+  ? Omit<{ [Property in keyof Type]: DeepOmit<Type[Property], Keys> }, Keys>
+  : Omit<Type, Keys>;
